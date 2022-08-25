66c65c3f4e441c6848b2b9adfb7bf1a650081753
tetlisna <tplovic@gmail.com>
2022-08-25 22:16:22 +0300

    finished 7

--- Homework7/js/homework7.js
+++ Homework7/js/homework7.js
@@ -5,8 +5,8 @@
 // 	2) із затримкою 2 сек змініть розміри вікна на 500х500 пікселів
 // 	3) із затримкою 2 сек перемістіть вікно в точку з координатами (200, 200)
 // 	4) із затримкою 2 сек закрийте вікно.
-let myWindow = window.open(" ", "new window", "_blank", width = 300, height = 300);
 
+let myWindow = window.open(" ", "new window", "_blank", width = 300, height = 300);
 setTimeout(() => {
   myWindow.resizeTo(500,500);
 }, 2000);
@@ -19,20 +19,20 @@ setTimeout(() => {
 
 // 2. Для заданої HTML-сторінки:
 
-// let textPar = document.querySelector("#text");
-// const btn = document.querySelector("button");
-// function changeCSS() {
-//   textPar.classList.add("change");
-  
-// }
+// напишіть функцію changeCSS(), яка спрацьовуватиме по кліку на кнопку і змінюватиме стиль вмісту тега <p>:
+//колір шрифту – оранжевий, розмір шрифту 20 пс, шрифт сімейства "Comic Sans MS".
 
-// <p id ='text'>I learning JavaScript events!</p> 
-// <div>
-// 	<button . . . . . >Change style!</button>
-// </div>
+const btn = document.querySelector("#change-style");
 
-// напишіть функцію changeCSS(), яка спрацьовуватиме по кліку на кнопку і змінюватиме стиль вмісту тега <p>: колір шрифту – оранжевий, розмір шрифту 20 пс, шрифт сімейства "Comic Sans MS".
+btn.addEventListener("click", function changeCSS() {
+  const textChange = document.querySelector("#text");
+  textChange.classList = "change";
+});
 
+{/* <p id ='text'>I learning JavaScript events!</p>
+<div>
+	<button . . . . . >Change style!</button>
+</div> */}
 
 // 3. Задано сторінку з 3 кнопками і 1 лінкою. Напишіть Javascript код і реалізуйте HTML-сторінку з відповідними подіями на кожному елементові:
 // 	1) 1-ша кнопка – при кліку на неї колір фону сторінки міняється на синій
@@ -40,38 +40,120 @@ setTimeout(() => {
 // 	3) 3-я кнопка – при натисненні і утримуванні кнопки колір фону сторінки стає коричневий. При відпусканні – білий.
 // 	4) При наведенні на лінку – колір фону стає жовтим, при відведенні – білим.
 // 	Приклад – курсор наведений на лінку.
- 
 
+let btn1 = document.querySelector("#btn1");
+let container = document.querySelector("#container");
+  btn1.addEventListener("click", function () {
+    document.body.style.backgroundColor = "blue";
+});
+
+let btn2 = document.querySelector("#btn2");
+  btn2.addEventListener("dblclick", function () {
+    document.body.style.backgroundColor = "pink";
+});
+let btn3 = document.querySelector("#btn3");
+  btn3.addEventListener("mousedown", function () {
+    document.body.style.backgroundColor = "brown";
+  btn3.addEventListener("mouseup", function () {
+    document.body.style.backgroundColor = "white";
+  });
+});
+
+let link = document.querySelector("#btn4");
+  link.addEventListener("mouseover", function () {
+    document.body.style.backgroundColor = "yellow";
+});
 
 // 4. Реалізуйте програму, яка по натисканню на кнопку видалятиме обраний елемент випадаючого списку. Можуть видалятися всі елементи в будь-якому порядку.
+let selectOpt = document.querySelector("#select");
 
- 
+function remove() {
+  selectOpt.remove(selectOpt.selectedIndex);
+}
+let selectBtn = document.querySelector("#btn-choose");
+  selectBtn.addEventListener("click", remove);
 
-
-
-// 5. Реалізуйте програму, яка по натисканню на кнопку виводитиме повідомлення 
+// 5. Реалізуйте програму, яка по натисканню на кнопку виводитиме повідомлення
 // "I was pressed!", при наведенні на кнопку виводитиме повідомлення "Mouse on me!", а при відведенні курсора миші виводитиме повідомлення "Mouse is not on me!".
 
-// Приклад роботи:
- 
+btnLive = document.querySelector("#btn-live");
 
+let p1 = document.querySelector("#mouseon");
+let p2 = document.querySelector("#press");
+let p3 = document.querySelector("#mouseout");
 
-// 6. Реалізуйте програму, яка відслідковуватиме зміну розміру (ширини і висоти) вікна браузера і виводитиме на поточну сторінку при її розтязі/стисканні відповідні значення.
+btnLive.onmouseover = function () {
+  p1.innerHTML = "Mouse on me!";
+};
 
- 
+btnLive.addEventListener("click", function () {
+  p1.innerHTML = "I was pressed!";
+});
 
+btnLive.addEventListener("mouseout", function () {
+  p1.innerHTML = "Mouse is not on me!";
+});
 
-// 7*. На сторінці потрібно реалізувати 2 випадаючих списки. У першому містяться назви країн, у другому – назви міст. Реалізувати роботу таким чином, щоб коли вибирається з лівого випадаючого списку певна країна - в правому випадаючому  списку з'являлися міста цієї країни. Список міст формується динамічно, через JavaScript. Також потрібно нижче вивести назву обраної країни і місто.
-   
+// 6. Реалізуйте програму, яка відслідковуватиме зміну розміру (ширини і висоти) вікна браузера і виводитиме на поточну сторінку при
+// її розтязі/стисканні відповідні значення.
+
+const heightOutput = document.querySelector("#height");
+const widthOutput = document.querySelector("#width");
+
+function sizeBrowser() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+window.addEventListener("resize", sizeBrowser);
+
+// 7*. На сторінці потрібно реалізувати 2 випадаючих списки. У першому містяться назви країн, у другому – назви міст. Реалізувати роботу таким чином, щоб
+// коли вибирається з лівого випадаючого списку певна країна - в правому випадаючому  списку з'являлися міста цієї країни. Список міст формується динамічно,
+// через JavaScript. Також потрібно нижче вивести назву обраної країни і місто.
 
 // Код HTML-сторінки:
-// <select name="country" id="country">
-// 	<option value="ger">Germany</option>
-// 	<option value="usa">USA</option>
-// 	<option value="ukr">Ukraine</option>
-// </select>
-                    
-// <select name="cities" id="cities"></select>
-// <p></p>
+/* <select name="country" id="country">
+	<option value="ger">Germany</option>
+	<option value="usa">USA</option>
+	<option value="ukr">Ukraine</option>
+</select>
 
+<select name="cities" id="cities"></select>
+<p></p> */
 
+const objectCountryCity = {
+  ukr: ["Kyiv", "Lviv", "Kharkiv"],
+  usa: ["New-York", "Orlando", "Los-Angeles"],
+  ger: ["Berlin", "Dresden", "Munich"],
+};
+
+let citiesList = document.querySelector("#cities");
+let countryList = document.querySelector("#country");
+let textShow = document.querySelector("#show");
+
+function generateCities() {
+  const selectedCountry = countryList.value;
+  const selectedOption = objectCountryCity[selectedCountry];
+  citiesList.innerHTML = "";
+
+  Array.from(selectedOption).forEach(function (el) {
+    let option = new Option(el);
+    citiesList.appendChild(option);
+  });
+  showSelected();
+}
+
+generateCities();
+
+countryList.addEventListener("change", function () {
+  generateCities();
+  showSelected();
+});
+citiesList.addEventListener("change", function () {
+  showSelected();
+});
+
+function showSelected() {
+  const selectedCity = citiesList.value;
+  let countryListOption = document.querySelectorAll("#country option")[countryList.selectedIndex].innerHTML;
+  textShow.innerHTML = " " + countryListOption + ", " + selectedCity;
+}
